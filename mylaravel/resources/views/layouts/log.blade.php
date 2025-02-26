520c011903e832e3c8e4bfd150eddef7960d4c2c
Sorayoot Soison <leo.sorayut9@gmail.com>
2025-02-26 22:23:26 +0700

    Login และ Dynamic Form
    การบ้าน Login และ Dynamic Form

--- mylaravel/resources/views/layouts/log.blade.php
+++ mylaravel/resources/views/layouts/log.blade.php
@@ -51,6 +51,10 @@
   <!--begin::Body-->
   <body class="login-page bg-body-secondary">
     @yield('content')
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" 
+      integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" 
+      crossorigin="anonymous">
+    </script>
     <!-- /.login-box -->
     <!--begin::Third Party Plugin(OverlayScrollbars)-->
     <script
--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -6,19 +6,32 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+use App\Http\Middleware\CheckLogin;
 
 
     Route::get('/home', 
-    [HomeController::class, 'index']);
+    [HomeController::class, 'index'])->middleware(CheckLogin::class);
 
     Route::get('/login',     
     [LoginController::class, 'index']);
+    
+    Route::post('/login',
+    [LoginController::class, 'login']);
+
+    Route::get('/logout', function(){
+        session()->forget('user');
+        session()->flush(); 
+        return redirect('/login');
+    });
+    
+    
 
     Route::get('/register',    
     [RegisterController::class, 'index']);
 
     Route::get('/', 
-    [HomeController::class, 'index']);
+    [HomeController::class, 'index'])->middleware(CheckLogin::class);
 
     Route::get('/500', 
     [HomeController::class, 'error500']);
@@ -26,17 +39,21 @@ use App\Http\Controllers\UserController;
     Route::post('/register', 
     [RegisterController::class, 'create']);
 
+Route::middleware([CheckLogin::class])->group(function(){
     Route::get('/users', 
     [UserController::class, 'index']);
-
     Route::get('/user/{id}', 
     [UserController::class, 'edit']);
-
     Route::put('/user', 
     [UserController::class, 'edit_action']);
-
     Route::delete('/user', 
     [UserController::class, 'delete']);
+
+    Route::get('/product',
+    [ProductController::class, 'index']);
+    Route::post('/product',
+    [ProductController::class, 'add_product']);
+});
 
 
 Route::post('/mycontroller', 
